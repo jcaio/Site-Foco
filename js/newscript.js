76ac691d6940f621b6207b573cae215a799bc9a3
Juan Caio <juancaiops@gmail.com>
2018-08-21 13:08:07 -0300

    Novo layout

---
+++ js/newscript.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    }
+  });
