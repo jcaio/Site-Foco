527d9701a7b6ab42a2fd03a8fff4438c7a995575
Juan Caio <juancaiops@gmail.com>
2018-09-03 12:43:38 -0300

    Atualização 03/09/2018

--- js/newscript.js
+++ js/newscript.js
@@ -1,10 +1,48 @@
+/*Slide Principal*/
 var swiper = new Swiper('.swiper-container', {
-    loop: false,
+    loop: true,
+    slidesPerView: 1,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     autoplay: {
         delay: 5000,
+    }, 
+    breakpoints:{
+      320:{
+        slidesPerView: 1
+      },
+      480:{
+        slidesPerView: 1
+      },
+      640:{
+        slidesPerView: 1
+      }
+    }
+  });
+
+  var swiper = new Swiper('.swipper-container-integracoes', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next-integracoes',
+      prevEl: '.swiper-button-prev-integracoes',
+    },
+    breakpoints: {
+      320:{
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      480:{
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      640:{
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
     }
   });
